aa4d3f0ea3f41b73081286a121254c69f5db7148
Thomas <substantialimpulse@pm.me>
2024-10-03 15:20:55 +0200

    fix(e2e): flaky login test

--- e2e/tests/login.setup.ts
+++ e2e/tests/login.setup.ts
@@ -9,11 +9,11 @@ test('Login', { tag: '@fast' }, async ({ page }) => {
   await page.getByLabel('Password').fill('testtest')
   await page.getByLabel('Password').press('Enter')
   await page.waitForURL('**/find')
-  await expect(page.getByRole('link', { name: 'REIMS Santa Ana' })).toBeInViewport()
 
   await page.waitForResponse(
     (response) => response.url().includes('/all') && response.status() === 200,
   )
   await expect(page.locator('div').filter({ hasText: 'Setting up REIMS2' })).toHaveCount(0)
+  await expect(page.getByRole('link', { name: 'REIMS Santa Ana' })).toBeInViewport()
   await page.context().storageState({ path: authFile })
 })
