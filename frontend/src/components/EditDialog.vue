65f983a4746916e4d821958f74f2d2afdd61b1e5
Thomas <substantialimpulse@pm.me>
2024-10-12 12:19:56 +0200

    feat(edit): improve edit dialog

--- frontend/src/components/EditDialog.vue
+++ frontend/src/components/EditDialog.vue
@@ -3,17 +3,14 @@
     <v-card>
       <v-toolbar
         color="primary"
-        :title="`Edit Glasses SKU ${glasses.sku?.toString().padStart(4, '0')}`"
+        :title="`Edit Glasses SKU ${originalGlasses.sku?.toString().padStart(4, '0')}`"
       ></v-toolbar>
       <v-card-text>
-        <glass-input
-          :model-value="glassesInput"
-          @update:model-value="(val) => updateGlasses(val)"
-        ></glass-input>
+        <glass-input v-model="glassesInput"></glass-input>
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn variant="text" @click="dialogOpen = false">Save</v-btn>
+        <v-btn variant="text" @click="updateGlasses()">Save</v-btn>
         <v-btn variant="text" @click="dialogOpen = false">Close</v-btn>
       </v-card-actions>
     </v-card>
@@ -24,26 +21,35 @@
 import { DisplayedEye, Eye, Glasses, GlassesInput } from '@/model/GlassesModel'
 import GlassInput from '@/components/GlassInput.vue'
 const dialogOpen = defineModel<boolean>('open', { required: true })
-const { glasses } = defineProps<{ glasses: Glasses }>()
+const originalGlasses = defineModel<Glasses>('glasses', { required: true })
 
-const glassesInput: Ref<GlassesInput> = computed(() => {
-  return {
-    ...glasses,
-    od: eyeToStringEye(glasses.od),
-    os: eyeToStringEye(glasses.os),
+const glassesInput: Ref<GlassesInput> = ref(convertGlassesToGlassesInput(originalGlasses.value))
+
+watch(dialogOpen, (open) => {
+  if (open) {
+    glassesInput.value = convertGlassesToGlassesInput(originalGlasses.value)
   }
 })
+
+function convertGlassesToGlassesInput(val: Glasses): GlassesInput {
+  return {
+    ...val,
+    od: eyeToStringEye(val.od),
+    os: eyeToStringEye(val.os),
+  }
+}
 
 function eyeToStringEye(eye: Eye): DisplayedEye {
   return {
     sphere: eye.sphere.toFixed(2),
     cylinder: eye.cylinder.toFixed(2),
-    axis: eye.axis.toFixed(0),
-    add: eye.add?.toString(2) ?? '',
+    axis: eye.axis.toString().padStart(3, '0'),
+    add: eye.add?.toFixed(2) ?? '',
   }
 }
 
-function updateGlasses(val: GlassesInput) {
+function updateGlasses() {
   // todo
+  dialogOpen.value = false
 }
 </script>
