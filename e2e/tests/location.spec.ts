f41f334fd76120b25783b8a157d7c227039f3953
Thomas <substantialimpulse@pm.me>
2024-10-25 18:43:50 +0200

    feat(e2e): add test for changing location

---
+++ e2e/tests/location.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+
+test('Switch location', { tag: '@fast' }, async ({ page }) => {
+  await page.goto('/find')
+
+  await page.getByText('Change location').click()
+  const dialog = page.getByRole('dialog')
+  await dialog.getByRole('combobox').click()
+  await page.getByRole('option', { name: 'San Miguel' }).click()
+  await dialog.getByRole('button', { name: 'Apply' }).click()
+  await expect(dialog.getByText('Change location')).toBeHidden()
+  await expect(page.getByRole('navigation')).toContainText('REIMS San Miguel')
+})
