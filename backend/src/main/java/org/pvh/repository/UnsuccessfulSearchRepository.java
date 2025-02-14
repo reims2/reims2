0626facbf4255d21be27c9a339b89cbd54bcf8eb
Thomas <substantialimpulse@pm.me>
2025-02-14 13:11:02 +0100

    feat: allow to export unsuccessful searches

--- backend/src/main/java/org/pvh/repository/UnsuccessfulSearchRepository.java
+++ backend/src/main/java/org/pvh/repository/UnsuccessfulSearchRepository.java
@@ -1,5 +1,7 @@
 package org.pvh.repository;
 
+import java.util.List;
+
 import org.pvh.model.entity.UnsuccessfulSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UnsuccessfulSearchRepository extends JpaRepository<UnsuccessfulSearch, Long> {
 
+    List<UnsuccessfulSearch> findAllByLocation(String location);
 }
