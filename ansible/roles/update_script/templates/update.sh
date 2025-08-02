519eeb1d424a7998b3445890db14d3155eed8880
Thomas <substantialimpulse@pm.me>
2025-08-02 20:20:32 +0200

    fix(ansible): service rollback discovery

--- ansible/roles/update_script/templates/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-{{ ansible_managed | comment }}
-set -e
-
-new_tag=$SSH_ORIGINAL_COMMAND
-
-docker service update {{ stack_name }}_frontend --image reims2/reims2-frontend:sha-"$new_tag" --force
-docker service update {{ stack_name }}_docs --image reims2/reims2-docs:sha-"$new_tag" --force
-docker service update {{ stack_name }}_backend --image reims2/reims2-backend:sha-"$new_tag" --force
